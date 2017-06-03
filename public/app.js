d5afb4d4613e6216b3dd0880348cfc85ee8a31ce
Spencer Balkin <spencer.balkin@gmail.com>
2017-06-03 19:53:07 -0400

    only thing left to do on homepage is fix overflow of text

--- public/app.js
+++ public/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+  $("#editButton").on("click", function() {
+    $(".editHide").css("display", "block");
+  });
+
+  $(".card").on("mouseenter", function() {
+    $(this).addClass("z-depth-5")
+  })
+  $(".card").on("mouseleave", function() {
+    $(this).removeClass("z-depth-5")
+  })
+
+
   $('#delete-button').on('click', () => {
      const postId = $('#delete-button').data('id');
 
@@ -26,4 +38,6 @@ $(document).ready(function(){
     .match(/age=(\d+)/)[1];
 
    $("#age_scale").val(ageGiven);
-})
+
+
+}) //end of jquery
